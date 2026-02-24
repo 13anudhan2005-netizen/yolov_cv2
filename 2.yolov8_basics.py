986c0bed74f1a98c5b00177b1823192fadb9e5fe
anudhan mohurle <13anudhan2005@gmail.com>
2026-02-24 22:09:43 +0530

    Add files via upload

---
+++ 2.yolov8_basics.py
@@ -0,0 +1,13 @@
+from ultralytics import YOLO
+import numpy
+
+# load a pretrained YOLOv8n model
+model = YOLO("yolov8n.pt", "v8")  
+
+# predict on an image
+detection_output = model.predict(source=r"C:\Users\mohur\Downloads\cars.jpg", conf=0.25, save=True) 
+# Display tensor array
+print(detection_output)
+
+# Display numpy array
+# print(detection_output[0].numpy())
